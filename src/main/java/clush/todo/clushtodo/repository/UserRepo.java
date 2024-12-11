739058c1355f95ff8f15730398426d0b41fde419
SURI <snyeg28842@gmail.com>
2024-12-11 15:46:19 +0900

    fix : 어노테이션 빼먹음

--- src/main/java/clush/todo/clushtodo/repository/UserRepo.java
+++ src/main/java/clush/todo/clushtodo/repository/UserRepo.java
@@ -2,9 +2,11 @@ package clush.todo.clushtodo.repository;
 
 import clush.todo.clushtodo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, String> {
     Optional<User> findByUserIdAndPassword(String id, String pw);
 }
