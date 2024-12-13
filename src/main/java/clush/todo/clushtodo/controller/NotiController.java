b45894fe1f3ab930e33509cea875e5d20925af02
SURI <snyeg28842@gmail.com>
2024-12-13 13:53:15 +0900

    fix: 의존성 분리 및 리팩토링

--- src/main/java/clush/todo/clushtodo/controller/NotiController.java
+++ src/main/java/clush/todo/clushtodo/controller/NotiController.java
@@ -1,6 +1,8 @@
 package clush.todo.clushtodo.controller;
 
 import clush.todo.clushtodo.entity.Notification;
+import clush.todo.clushtodo.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,26 +17,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/sse")
 public class NotiController {
-
-    private final Map<String,SseEmitter> emitters = new HashMap<>();
+    @Autowired
+    NotificationService notiSvc;
     @GetMapping("/connect")
     public SseEmitter connect(@RequestParam("userId") String userId) {
-        SseEmitter emitter = new SseEmitter();
-        emitters.put(userId,emitter);
-
-        emitter.onCompletion(() -> emitters.remove(emitter));
-        emitter.onTimeout(() -> emitters.remove(emitter));
-
-        return emitter;
-    }
-
-    public void sendNotification(Notification noti) {
-
-        try {
-            emitters.get(noti.getUser().getUserId()).send(SseEmitter.event().name("알림").data(noti.getContent()));
-        } catch (IOException e) {
-            emitters.remove(emitters.get(noti.getUser().getUserId()));
-        }
+        return notiSvc.connect(userId);
     }
 
 }
--- src/main/java/clush/todo/clushtodo/service/NotificationService.java
+++ src/main/java/clush/todo/clushtodo/service/NotificationService.java
@@ -1,6 +1,7 @@
 package clush.todo.clushtodo.service;
 
 import clush.todo.clushtodo.controller.NotiController;
+import clush.todo.clushtodo.dto.Noti;
 import clush.todo.clushtodo.entity.Notification;
 import clush.todo.clushtodo.repository.NotiRepo;
 import lombok.RequiredArgsConstructor;
@@ -8,27 +9,57 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service@Slf4j@RequiredArgsConstructor
 public class NotificationService {
 
+    //서비스를 위해 확장한다면 다른 저장공간을 필요로 함.-> 애플리케이션 종료/재시작 시 사라짐.
+    private final Map<String,SseEmitter> emitters = new HashMap<>();
     @Autowired
     private NotiRepo notiRepo;
 
-    @Autowired
-    private NotiController notiController;
-
+    // 매 분마다 보내야할 알림을 체크하며 전송
     @Scheduled(fixedRate = 60000)
     public void checkAndSendNotifications() {
         LocalDateTime now = LocalDateTime.now();
 
         List<Notification> notifications = notiRepo.findByRingBeforeAndSentFalse(now);
         for (Notification notification : notifications) {
-            notiController.sendNotification(notification);
-            notiRepo.updateSent(notification.getNid());
+            sendNotification(notification);
         }
+    }
+
+    //연결된 유저는 보내고, 안되면 연결된 유저 리스트에서 삭제함
+    public void sendNotification(Notification noti) {
+
+        try {
+            emitters.get(noti.getUser().getUserId()).send(SseEmitter.event().name("알림").data(noti.getContent()));
+            notiRepo.updateSent(noti.getNid());
+        } catch (IOException e) {
+            emitters.remove(emitters.get(noti.getUser().getUserId()));
+        }
+    }
+
+    // 할일이 사라지기 10분전 정리 시간을 경고,
+    @Scheduled(cron = "50 3 * * * *" )
+    public void notifyUncompletedTodo() {
+        sendNotification();
+    }
+
+    public SseEmitter connect(String userId) {
+        SseEmitter emitter = new SseEmitter();
+        emitters.put(userId,emitter);
+
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitter.onTimeout(() -> emitters.remove(emitter));
+
+        return emitter;
     }
 }
