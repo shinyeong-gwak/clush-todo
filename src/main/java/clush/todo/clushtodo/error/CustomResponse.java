828f90ad4fd8fd3076e4d8c93ab768e3926e6fbb
SURI <snyeg28842@gmail.com>
2024-12-13 14:34:27 +0900

    fix : 예외 추가

--- src/main/java/clush/todo/clushtodo/error/CustomResponse.java
+++ src/main/java/clush/todo/clushtodo/error/CustomResponse.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum CustomResponse {
     UNAUTHORIZED(401, "로그인되지 않았어요."),
     NOT_FOUND(404,"찾는 데이터가 없어요."),
+    DATA_NOT_SAVED(404,"데이터를 저장하지 못했어요."),
     INVALID_CREDENTIALS(401, "아이디/비밀번호를 확인하세요.");
     int code;
     String message;
