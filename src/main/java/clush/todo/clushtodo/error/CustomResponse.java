26e0ca37828086d0352d60fa152745cd6e624ebc
SURI <snyeg28842@gmail.com>
2024-12-19 17:47:24 +0900

    fix: 새로운 타입

--- src/main/java/clush/todo/clushtodo/error/CustomResponse.java
+++ src/main/java/clush/todo/clushtodo/error/CustomResponse.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CustomResponse {
     UNAUTHORIZED(401, "로그인되지 않았어요."),
+    ALREADY_EXISTS(404, "이미 존재하기 때문에 요청이 실패했어요."),
     NOT_FOUND(404,"찾는 데이터가 없어요."),
     DATA_NOT_SAVED(404,"데이터를 저장하지 못했어요."),
     INVALID_CREDENTIALS(401, "아이디/비밀번호를 확인하세요."),
